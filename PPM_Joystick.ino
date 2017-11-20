d1aafdc5ff4de5c629e5eb5974fea0a9f4e2a5dc
Unknown <andrew@hux.net.au>
2017-11-21 09:40:15 +1030

    Initial
    Initial Comit

---
+++ PPM_Joystick.ino
@@ -0,0 +1,65 @@
+/*This program puts the servo values into an array,
+ reagrdless of channel number, polarity, ppm frame length, etc...
+ You can even change these while scanning!*/
+ 
+
+#define PPM_Pin 3  //this must be 2 or 3
+#define multiplier (F_CPU/8000000)  //leave this alone
+int ppm[16];  //array for storing up to 16 servo signals
+
+void setup()
+{
+  pinMode(A1, INPUT_PULLUP);  // Initialize the button pin
+  
+  //Serial.begin(115200);
+  //Serial.println("ready");
+
+  pinMode(PPM_Pin, INPUT);
+
+  attachInterrupt(PPM_Pin - 2, read_ppm, CHANGE);
+
+  TCCR1A = 0;  //reset timer1
+  TCCR1B = 0;
+  TCCR1B |= (1 << CS11);  //set timer1 to increment every 0,5 us
+  Joystick.begin();  // init joystick with 'autosendvalues' enabled
+}
+
+void loop()
+{
+	  // CH 1 
+    Joystick.setXAxis(map(ppm[0],1000,2000, -127, 127));
+    // CH 2 
+    Joystick.setYAxis(map(ppm[1],1000,2000, -127, 127));
+    // CH 3 
+    Joystick.setZAxis(map(ppm[2],1000,2000, -127, 127));
+    // CH 4 (PIN 16)
+    //Joystick.setXAxisRotation(map(rc_values[3],rc_min_values[3],rc_max_values[3], 0, 360));
+    // CH 5 (PIN 14)
+    Joystick.setThrottle(map(ppm[3],1000,2000, 0, 255));
+    // CH 6 (PIN 15)
+    Joystick.setRudder(map(ppm[4],1000,2000, 0, 255));
+}
+
+
+
+void read_ppm(){  //leave this alone
+  static unsigned int pulse;
+  static unsigned long counter;
+  static byte channel;
+  static unsigned long last_micros;
+
+  counter = TCNT1;
+  TCNT1 = 0;
+
+  if(counter < 710*multiplier){  //must be a pulse if less than 710us
+    pulse = counter;
+  }
+  else if(counter > 1910*multiplier){  //sync pulses over 1910us
+    channel = 0;
+  }
+  else{  //servo values between 710us and 2420us will end up here
+    ppm[channel] = (counter + pulse)/multiplier;
+    channel++;
+  }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Arduino_PPM_Joystick
 RC PPM signal to Joystick interface for wireless transmitter use in simulators etc.
+
+Based on the code from http://code.google.com/p/read-any-ppm and added the USB features.
+
+Requires Arduino 1.6.5 or lower (newer versions implement USB Joystick differently)
+You will need the updated USB profiles (USBAPI.h and HID.cpp) to be placed in the Arduino\hardware\arduino\avr\cores\arduino folder.
+
+Write to an arduino Pro Micro (Not Mini or Uno/Mega) or equivalent such as Freetronics Leostick or Arduino Leonardo with the Atmega32u4
+
+Connect PPM out from RX to Pin 3 of the Arduino (must be an interrupt pin)
+
+Program as normal.
